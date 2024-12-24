3d1d6e98fd323ce63d96d0b973220fbe112d6b9d
tharun <tharuntk143143@gmail.com>
2024-12-24 21:47:09 +0430

    DF

--- ecommercebackend/server.js
+++ ecommercebackend/server.js
@@ -1,61 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
-const serverless = require('serverless-http'); // Required for Vercel
-
-// Import routes
-const productRoutes = require('./routes/productRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const saleRoutes = require('./routes/saleRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-
-// Initialize the app
 const app = express();
 
-// Middleware setup
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-
-// Serve static files from the uploads folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Multer configuration for handling file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'Server is running!' });
 });
-const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } }); // 5MB file limit
 
-// Routes
-app.use('/api/admin', adminRoutes);
-app.use('/api/sales', saleRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/products', productRoutes);
-
-// MongoDB Connection URI
-const MONGO_URI = process.env.MONGO_URI;
-
-// Connect to MongoDB
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-// Export app for serverless function
-module.exports = serverless(app); // Required for Vercel 
+module.exports = app;
