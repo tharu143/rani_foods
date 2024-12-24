4888598bcde59c07b6a8b3c6595e5ace04886afa
tharun <tharuntk143143@gmail.com>
2024-12-24 21:52:47 +0430

    ss

--- ecommercebackend/server.js
+++ ecommercebackend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
 const serverless = require('serverless-http');
 
 // Import routes
@@ -18,23 +15,6 @@ const app = express();
 // Middleware setup
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-
-// Serve static files from the uploads folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Multer configuration for handling file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-
-const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } }); // 5MB file limit
 
 // Routes
 app.use('/api/admin', adminRoutes);
